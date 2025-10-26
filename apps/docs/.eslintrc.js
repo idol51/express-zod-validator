f92a517217a3b3af3837671f7b401cb7f69a0678
idol51 <armaanlodi.123@gmail.com>
2025-10-26 16:17:45 +0530

    feat: migrate from @acme to @idol51 namespace and update configurations

--- apps/docs/.eslintrc.js
+++ apps/docs/.eslintrc.js
@@ -1,6 +1,6 @@
 module.exports = {
   root: true,
-  extends: ["@acme/eslint-config/next.js"],
+  extends: ["@idol51/eslint-config/next.js"],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     project: true,
